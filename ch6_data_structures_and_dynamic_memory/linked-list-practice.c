fae1abf16fc9ccd7187cd0676a592b30b1372cc8
John Stegmaier <jupdown@protonmail.com>
2026-01-15 22:03:46 -0500

    Convert linked list practice exercise into dynamic person generation from stdin with hacks (and it still needs to be finished next week)

--- ch6_data_structures_and_dynamic_memory/linked-list-practice.c
+++ ch6_data_structures_and_dynamic_memory/linked-list-practice.c
@@ -1,40 +1,65 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct person {
+    char *name;
+    int age;
+} person;
+
+typedef struct personNode
+{
+    person this;
+    struct personNode *next;
+} personNode;
+
+char *getName()
+{
+    char *name = malloc(sizeof(char) * 100);
+    if (name == NULL)
+    {
+        fprintf(stderr, "Failed to allocate memory for name");
+        exit(1);
+    }
+    printf("MY GRANDMA IS BEING CRUSHED BY A SERVER. PLEASE ENTER A PERSON'S NAME SO SHE CAN BE SAVED:\n");
+    fgets(name, sizeof(name), stdin);
+    return name;
+}
+
+int getAge()
+{
+    char buffer[12];
+    printf("Please enter their age:\n");
+    fgets(buffer, sizeof(buffer), stdin);
+    int age = strtol(buffer, nullptr, 10);
+    return age;
+}
+
+person *buildPerson()
+{
+    char *personsName = getName();
+    int age = getAge();
+    person *aPerson = malloc(sizeof(person));
+    if (aPerson == NULL)
+    {
+        fprintf(stderr, "Failed to allocate memory for person");
+        exit(1);
+    }
+    aPerson->name = personsName;
+    aPerson->age = age;
+    printf("You have created a person with the name %s and age %d\n", aPerson->name, aPerson->age);
+    return aPerson;
+}
 
 int main(void) {
-    // Recap: how to declare a struct
-    struct point {
-        int x;
-        int y;
-    };
-    struct point coord = {3, 5};
-    printf("x: %d y: %d\n", coord.x, coord.y);
-
     // Declare a person type - we'll use this as a linked list node
-    typedef struct person {
-        char *name;
-        int age;
-        struct person *next;
-    } person;
+    char input = ' ';
 
-    // Print the fields on the first node
-    person cam = {"Cam", 83, NULL};
-    printf("Meet %s age %d\n", cam.name, cam.age);
-    printf("Meet %s\n", (char*)&cam); // doesn't work, wamp wamp
-
-    // Assign the next node in the list
-    person nick = { "Nick" , 42, NULL};
-    cam.next = &nick;
-    printf("Meet Cam's %s age %d\n", cam.next->name, cam.next->age);
-
-    // Assign the third node in the list
-    person jason = { "Jason", 21, NULL};
-    nick.next = &jason;
-
-    // Walk the entire list
-    for (person *thisGuy = &cam; thisGuy != NULL;) {
-        printf("This is %s\n", thisGuy->name);
-        thisGuy = thisGuy->next;
+    while (input != 'Q')
+    {
+        person *newPerson = buildPerson();
+        printf("Add another person? (Y)es, (N)o, (Q)uit\n");
+        input = (char) fgetc(stdin);
+        fgetc(stdin); // this is a horrible hacky solution!!!!!!!!!!!
     }
-
     return 0;
 };
